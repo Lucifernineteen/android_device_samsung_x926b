14a34ce660cdb7b5f3e8fe304a31169193c6a21b
Lucifernineteen <155066517+Lucifernineteen@users.noreply.github.com>
2025-10-26 04:30:36 +0530

    Update lunch choices for twrp_x926b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,7 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_x926b.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_x926b-eng \
-    twrp_x926b-userdebug \
-    twrp_x926b-user
-    
+    twrp_x926b-trunk_staging-eng \
+    twrp_x926b-trunk_staging-userdebug \
+    twrp_x926b-trunk_staging-user

e9f90e1f33949b6161c25d20145a5cf8d424955e
Lucifernineteen <155066517+Lucifernineteen@users.noreply.github.com>
2025-10-26 04:32:23 +0530

    Update twrp_x926b.mk by removing overrides
    Removed product name and default property overrides.

--- twrp_x926b.mk
+++ twrp_x926b.mk
@@ -18,10 +18,5 @@ PRODUCT_MODEL := SM-X926B
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=gts10uxx \
-    BUILD_FINGERPRINT="samsung/gts10uxx/x926b:15/AP31.240905.052/X926BXXS4BYH1:user/release-keys"
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0
-    
+    PRIVATE_BUILD_DESC="twrp_x926b-trunk_staging-eng"
+  
